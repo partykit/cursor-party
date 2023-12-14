6f9a75a93b4e306c0a92fa038b692b97a7cda1e1
Sunil Pai <threepointone@gmail.com>
2023-12-14 11:49:15 +0000

    how to use css modules

--- src/cursors.tsx
+++ src/cursors.tsx
@@ -3,6 +3,19 @@ import { render } from "react-dom";
 import PresenceProvider from "./presence/presence-context";
 import Cursors from "./presence/Cursors";
 
+// @ts-ignore
+import classes from "./styles.module.css";
+
+// declare everything imported from *.module.css to be strings
+
+// // This doesn't appear to work right now, hence the @ts-ignore up there
+// declare module "*.module.css" {
+//   const classes: { [key: string]: string };
+//   export default classes;
+// }
+
+console.log(classes);
+
 declare const PARTYKIT_HOST: string;
 
 const pageId = window?.location.href
